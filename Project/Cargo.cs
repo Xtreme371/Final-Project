015a2a955c6db8aa928e73fedfdc706d627a6c30
Shelton <shelton.steyn@gmail.com>
2019-07-23 18:06:23 +0200

    Added all the classes

---
+++ Project/Cargo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Cargo
+    {
+        private int id;
+        private string name;
+        private string type;
+        private double weight;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Type { get => type; set => type = value; }
+        public double Weight { get => weight; set => weight = value; }
+
+        public Cargo(int id, string name, string type, double weight)
+        {
+            this.id = id;
+            this.name = name;
+            this.type = type;
+            this.weight = weight;
+        }
+        public Cargo()
+        {
+
+        }
+    }
+}
---
+++ Project/Plane.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Plane
+    {
+        private int id;
+        private string name;
+        private string type;
+        private double fuel;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Type { get => type; set => type = value; }
+        public double Fuel { get => fuel; set => fuel = value; }
+
+        public Plane(int id, string name, string type, double fuel)
+        {
+            this.id = id;
+            this.name = name;
+            this.type = type;
+            this.fuel = fuel;
+        }
+        public Plane()
+        {
+
+        }
+    }
+}
