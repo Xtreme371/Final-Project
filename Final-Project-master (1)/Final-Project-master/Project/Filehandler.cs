a965eee9662b1eb648311f955a214e6af40f0b2f
pearl090 <53219737+pearl090@users.noreply.github.com>
2019-07-24 16:22:45 +0200

    Add files via upload

---
+++ Final-Project-master (1)/Final-Project-master/Project/Filehandler.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Project
+{
+    class Filehandler
+    {
+        FileStream stream;
+        StreamReader reader;
+        StreamWriter writer;
+
+        // READ USERS FROM FILE
+        public List<string> ReadUsers(string fileName)
+        {
+            List<string> data = new List<string>();     
+            try
+            {
+                stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                reader = new StreamReader(stream);
+
+                string line = string.Empty;
+                while ((line=reader.ReadLine())!=null)
+                {
+                    data.Add(line);
+                }
+
+            }
+            catch (FileNotFoundException fnfe)
+            {
+
+                MessageBox.Show(fnfe.Message);
+            }
+            catch (DirectoryNotFoundException dnfe)
+            {
+
+                MessageBox.Show(dnfe.Message);
+            }
+            finally
+            {
+                stream.Flush();
+                reader.Close();
+                stream.Close();
+            }
+            return data;
+            // REGISTER NEW USER
+            
+        }
+        public List<string> WriteData(string fileName,string dataToWrite)
+        {
+            List<string> writedata = new List<string>();
+
+            try
+            {
+                stream = new FileStream(fileName, FileMode.Append, FileAccess.Write);
+                writer = new StreamWriter(stream);
+
+                foreach (var item in dataToWrite)
+                {
+                    writer.WriteLine(item);
+                }
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                MessageBox.Show(fnfe.Message);
+            }
+            catch (DirectoryNotFoundException dnfe)
+            {
+
+                MessageBox.Show(dnfe.Message);
+            }
+            finally
+            {
+                writer.Flush();
+                writer.Close();
+                stream.Close();
+            }
+            return writedata;
+        }
+    }
+}
---
+++ Final-Project-master (1)/Final-Project-master/Project/Objects.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Objects
+    {
+        private string id;
+        private string type;
+        private int threatlevel;
+        DateTime timeidentified;
+
+        public string Id { get => id; set => id = value; }
+        public string Type { get => type; set => type = value; }
+        public int Threatlevel { get => threatlevel; set => threatlevel = value; }
+        public DateTime Timeidentified { get => timeidentified; set => timeidentified = value; }
+
+        public Objects(string id, string type, int threatlevel, DateTime timeidentified)
+        {
+            this.id = id;
+            this.type = type;
+            this.threatlevel = threatlevel;
+            this.timeidentified = timeidentified;
+        }
+        public Objects()
+        {
+
+        }
+    }
+}
